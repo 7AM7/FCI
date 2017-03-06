c1f5e529df23e05f88547da2888f12a2f23d51b7
7AM7 <gentlemanana1@gmail.com>
2017-03-06 13:01:29 +0200

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.Widget;
+using SupportFragment = Android.Support.V4.App.Fragment;
+using SupportFragmentManager = Android.Support.V4.App.FragmentManager;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using SupportActionBar = Android.Support.V7.App.ActionBar;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+using System.Collections.Generic;
+using Java.Lang;
+using DesignLibrary_Tutorial.Fragments;
+using DesignLibrary_Tutorial.Helpers;
+
+namespace DesignLibrary_Tutorial
+{
+    [Activity(Label = "DesignLibrary_Tutorial", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Theme.DesignDemo")]
+    public class MainActivity : AppCompatActivity
+    {
+        private DrawerLayout mDrawerLayout;
+        public event EventHandler OptionButtonWasClicked;
+       //public event MenuItemOnMenuItemClickEventArgs clicking;
+            ViewPager viewPager;
+        // SupportFragmentManager transaction1;
+        NavigationView navigationView;
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+         
+
+            SupportToolbar toolBar = FindViewById<SupportToolbar>(Resource.Id.toolBar);
+            SetSupportActionBar(toolBar);
+
+            SupportActionBar ab = SupportActionBar;
+            ab.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+            ab.SetDisplayHomeAsUpEnabled(true);
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            if (navigationView != null)
+            {
+                SetUpDrawerContent(navigationView);
+
+            }
+           
+            TabLayout tabs = FindViewById<TabLayout>(Resource.Id.tabs);
+
+            viewPager = FindViewById<ViewPager>(Resource.Id.viewpager);
+            
+            SetUpViewPager(viewPager);
+
+            tabs.SetupWithViewPager(viewPager);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+
+            fab.Click += (o, e) =>
+            {
+     
+                            //Do something here
+                            Intent intent = new Intent(fab.Context, typeof(BottomSheetActivity));
+                            StartActivity(intent);
+  
+            };
+        }
+
+        private void SetUpViewPager(ViewPager viewPager)
+        {
+            TabAdapter adapter = new TabAdapter(SupportFragmentManager);
+            adapter.AddFragment(new Fragment1(), "الرئيسية");
+            adapter.AddFragment(new Fragment2(), "المفضلة");
+            adapter.AddFragment(new Fragment3(), "الإضافة");
+            viewPager.Adapter = adapter;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    mDrawerLayout.OpenDrawer((int)GravityFlags.Left);
+                    return true;
+  
+                default:
+                    return base.OnOptionsItemSelected(item);                    
+            }
+        }
+
+
+
+
+        public void SetUpDrawerContent(NavigationView navigationView)
+        {
+
+            navigationView.NavigationItemSelected += (object sender, NavigationView.NavigationItemSelectedEventArgs e) =>
+            {
+                var Opition = OptionButtonWasClicked;
+                if (Opition != null)
+                {
+                    this.OptionButtonWasClicked.Invoke(this, e);
+                }
+                mDrawerLayout.CloseDrawers();
+            };
+        }
+
+
+        public class TabAdapter : FragmentPagerAdapter
+        {
+            public List<SupportFragment> Fragments { get; set; }
+            public List<string> FragmentNames { get; set; }
+
+            public TabAdapter (SupportFragmentManager sfm) : base (sfm)
+            {
+                Fragments = new List<SupportFragment>();
+                FragmentNames = new List<string>();
+            }
+
+            public void AddFragment(SupportFragment fragment, string name)
+            {
+                Fragments.Add(fragment);
+                FragmentNames.Add(name);
+            }
+
+            public override int Count
+            {
+                get
+                {
+                    return Fragments.Count;
+                }
+            }
+
+            public override SupportFragment GetItem(int position)
+            {
+
+                return Fragments[position];
+            }
+
+            public override ICharSequence GetPageTitleFormatted(int position)
+            {
+                return new Java.Lang.String(FragmentNames[position]);
+            }
+        }
+    }
+}
+
